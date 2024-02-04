d4195f1bfc16a49ca9c12ece9fad8f18f1e2b193
zennon-sml <zennonsampaioml@gmail.com>
2024-02-04 16:37:35 -0300

    createtodo and createpage working

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -1,3 +1,48 @@
+import { prisma } from "@/src/db";
+import Link from "next/link";
+import { redirect } from "next/navigation";
+
+async function createTodo(data: FormData) {
+    "use server"
+
+    const title = data.get("title")?.valueOf()
+    if (typeof title !== "string" || title.length === 0) {
+        throw new Error("Titulo Inválido")
+    }
+
+    await prisma.todo.create({data: {title, complete: false}})
+    redirect("/")
+}
+
 export default function Page() {
-    return <h1>opa dnv</h1>
+  return (
+    <>
+      <h1 className="text-2x1">Novo</h1>
+      <form className="flex gap-2 flex-col">
+        <input
+          type="text"
+          name="title"
+          className="border border-slate-300 bg-transparent rounded px-2 py-1 
+            outline-none focus-within:border-indigo-800"
+        />
+        <div className="flex gap-1 justify-end">
+          <Link
+            href=".."
+            className="border border-slate-300 text-slate-300 px-2 py-1 rounded
+       hoveri:bg-slate-700 focus-within:bg-slate-100 outline-none"
+          >
+            Cancelar
+          </Link>
+          <button
+            formAction={createTodo}
+            type="submit"
+            className="border border-slate-300 text-slate-300 px-2 py-1 rounded
+            hover:bg-slate-700 focus-within:bg-slate-100 outline-none"
+          >
+            Criar
+          </button>
+        </div>
+      </form>
+    </>
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,22 +3,29 @@ import { prisma } from "../db";
 import { TodoItem } from "../components/TodoItem";
 
 function getTodos() {
-  return prisma.todo.findMany()
+  return prisma.todo.findMany();
   //  await prisma.todo.create({data: {title: "test2", complete: false}})
 }
 
 export default async function Home() {
-  const todos = await getTodos()
-  return <>
-    <header className=" flex justify-between items-center mb-4">
-      <h1 className="text-2x1">Afazeres</h1>
-      <Link className="border border-slate-300 text-slate-300 px-2 py-1 rounded
-       hover:bg-slate-700 focus-within:bg-slate-100 outline-none" href="/new">Novo</Link>
-    </header>
-    <ul className="pl-4">
-      {todos.map(todo => (
-        <TodoItem key={todo.id} {...todo}/>
-      ))}
-    </ul>
-  </>
+  const todos = await getTodos();
+  return (
+    <>
+      <header className=" flex justify-between items-center mb-4">
+        <h1 className="text-2x1">Afazeres</h1>
+        <Link
+          className="border border-slate-300 text-slate-300 px-2 py-1 rounded
+       hover:bg-slate-700 focus-within:bg-slate-100 outline-none"
+          href="/new"
+        >
+          Novo
+        </Link>
+      </header>
+      <ul className="pl-4">
+        {todos.map((todo) => (
+          <TodoItem key={todo.id} {...todo} />
+        ))}
+      </ul>
+    </>
+  );
 }
