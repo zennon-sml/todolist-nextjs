7b783326ddadf1219ab6e6fc5b083b5a9c6c74f7
zennon-sml <zennonsampaioml@gmail.com>
2024-02-05 21:50:52 -0300

    error to conect to mongo mongoserverselectionerror

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,47 +1,16 @@
 import Link from "next/link";
-import { prisma } from "../db";
 import { TodoItem } from "../components/TodoItem";
-import axios from "axios";
+import connectToDatabase from "../mongodb";
 
-function getTodos() {
-  return prisma.todo.findMany();
-  //  await prisma.todo.create({data: {title: "test2", complete: false}})
-}
 
-async function toggleTodo(id: string, complete: boolean) {
-  "use server"
-
-  await prisma.todo.update({where: {id}, data: { complete }})
-}
-// testing axios
-async function getId(){
-    const res = await axios.get("https://api.github.com/users/zennon-sml")
-    const id = res.data.id
-    console.log(id)
-  
-}
-
-getId()
 
 export default async function Home() {
-  const todos = await getTodos();
-  return (
-    <>
-      <header className=" flex justify-between items-center mb-4">
-        <h1 className="text-2x1">Afazeres</h1>
-        <Link
-          className="border border-slate-300 text-slate-300 px-2 py-1 rounded
-       hover:bg-slate-700 focus-within:bg-slate-100 outline-none"
-          href="/new"
-        >
-          Novo
-        </Link>
-      </header>
-      <ul className="pl-4">
-        {todos.map((todo) => (
-          <TodoItem key={todo.id} {...todo} toggleTodo={toggleTodo}/>
-        ))}
-      </ul>
-    </>
-  );
+  try {
+    const db = await connectToDatabase();
+
+    console.log("vrau")
+  } catch (error) {
+    console.error('Error in API route:', error);
+  }
+  return <h1>mongo</h1>
 }
---
+++ src/mongodb.ts
@@ -0,0 +1,15 @@
+import { MongoClient } from "mongodb";
+async function connectToDatabase() {
+  const client = new MongoClient("mongodb+srv://zennon-sml:asdf@todolistcluster.zyftt99.mongodb.net/?retryWrites=true&w=majority", {});
+
+  try {
+    await client.connect();
+    console.log('Connected to MongoDB');
+    return client.db();
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error);
+    throw error;
+  }
+}
+
+export default connectToDatabase
